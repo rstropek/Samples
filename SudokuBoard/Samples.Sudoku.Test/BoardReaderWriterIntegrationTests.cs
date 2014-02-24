ea21bf3b956bedc773c86a28971e5ad2b6ef1497
Rainer Stropek <rainer@timecockpit.com>
2014-02-24 08:21:05 +0100

    Added BoardReader

---
+++ SudokuBoard/Samples.Sudoku.Test/BoardReaderWriterIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Samples.Sudoku.Test
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using System.IO;
+	using System.Linq;
+	using System.Reflection;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// Integration tests for reading/writing board data.
+	/// </summary>
+	[TestClass]
+	public class BoardReaderWriterIntegrationTests
+	{
+		public TestContext TestContext { get; set; }
+
+		[TestMethod]
+		[TestCategory("Integration")]
+		public async Task TestLoadBoardFromFile()
+		{
+			const string sampleBoardName = "SampleBoard";
+
+			var directory = Path.Combine(this.TestContext.TestDir, "Boards");
+			await Task.Run(() => Directory.CreateDirectory(directory));
+			using (var stream = new FileStream(Path.Combine(directory, sampleBoardName), FileMode.CreateNew))
+			{
+				await stream.WriteAsync(BoardSampleData.sampleBoard, 0, BoardSampleData.sampleBoard.Length);
+			}
+
+			var board = await BoardReader.LoadFromFileAsync(sampleBoardName, directory);
+			Assert.IsTrue(((byte[])board).SequenceEqual(BoardSampleData.sampleBoard));
+		}
+	}
+}
---
+++ SudokuBoard/Samples.Sudoku/BoardReader.cs
@@ -0,0 +1,54 @@
+﻿namespace Samples.Sudoku
+{
+	using Microsoft.WindowsAzure.Storage;
+	using Microsoft.WindowsAzure.Storage.Auth;
+	using System.Diagnostics.Contracts;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// Implements helper methods to load board data.
+	/// </summary>
+	public static class BoardReader
+	{
+		/// <summary>
+		/// Loads board from file asynchronously.
+		/// </summary>
+		/// <param name="boardName">Name of the board.</param>
+		/// <returns>
+		/// A task that represents the asynchronous operation. The value of the 
+		/// TResult parameter contains the loaded board.
+		/// </returns>
+		public static Task<Board> LoadFromFileAsync(string boardName, string boardsDirectory = null)
+		{
+			ContractExtensions.IsNotNull(boardName, "boardName");
+			Contract.EndContractBlock();
+
+			var streamManager = boardsDirectory == null ? new FileStreamManager() : new FileStreamManager(boardsDirectory);
+			var repository = new BoardStreamRepository(streamManager);
+			return repository.LoadAsync(boardName);
+		}
+
+		/// <summary>
+		/// Loads board from Azure Blob Storage asynchronously.
+		/// </summary>
+		/// <param name="boardName">Name of the board.</param>
+		/// <returns>
+		/// A task that represents the asynchronous operation. The value of the 
+		/// TResult parameter contains the loaded board.
+		/// </returns>
+		public static Task<Board> LoadFromBlobStorageAsync(string accountName, string accountKey, string containerName, string boardName)
+		{
+			ContractExtensions.IsNotNull(accountName, "accountName");
+			ContractExtensions.IsNotNull(accountKey, "accountKey");
+			ContractExtensions.IsNotNull(containerName, "containerName");
+			Contract.EndContractBlock();
+
+			var credentials = new StorageCredentials(accountName, accountKey);
+			var account = new CloudStorageAccount(credentials, true);
+			var client = account.CreateCloudBlobClient();
+			var container = client.GetContainerReference(containerName);
+			var repository = new BoardStreamRepository(new CloudBlobStreamManager(container));
+			return repository.LoadAsync(boardName);
+		}
+	}
+}
