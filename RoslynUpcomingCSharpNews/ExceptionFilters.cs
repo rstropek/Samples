ca3d9b074d424f05abc9f262466ebc2c929d7566
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2014-04-07 21:46:36 +0200

    Added Roslyn Preview Samples

---
+++ RoslynUpcomingCSharpNews/ExceptionFilters.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RoslynUpcomingCSharpNews
+{
+    // BTW - Note use of primary constructor here
+    public class DataAccessException(string message, public bool IsCritical) 
+        : Exception(message)
+    {
+    }
+
+    public static class DataAccess
+    {
+        private static void UpdateInternal(bool fastMode)
+        {
+            if (fastMode)
+            {
+                // Let's assume something bad happened in "fast mode" ...
+                throw new DataAccessException("Something bad happened!", true);
+            }
+            else
+            {
+                // Let's assume everything is OK in "normal" mode ...
+            }
+        }
+
+        public static void Update_OldRethrowing()
+        {
+            try
+            {
+                UpdateInternal(fastMode: false);
+                UpdateInternal(fastMode: true);
+            }
+            catch (DataAccessException dae) 
+            {
+                if (!dae.IsCritical)
+                {
+                    // Log and handle (e.g. retry) non-critical errors
+                }
+                else
+                {
+                    // We re-throw critical errors
+                    throw;
+                }
+            }
+        }
+
+        public static void Update_RethrowingWithExceptionFilter()
+        {
+            try
+            {
+                UpdateInternal(fastMode: false);
+                UpdateInternal(fastMode: true);
+            }
+            catch (DataAccessException dae) if (!dae.IsCritical)
+            {
+                // Log and handle (e.g. retry) non-critical errors
+            }
+        }
+    }
+
+
+    [TestClass]
+    public class ExceptionFilters
+    {
+        [TestMethod]
+        public void TestExceptionFilter()
+        {
+            string oldStackTrace;
+            string newStackTrace;
+
+            try
+            {
+                DataAccess.Update_OldRethrowing();
+            }
+            catch (DataAccessException dae)
+            {
+                oldStackTrace = dae.StackTrace;
+            }
+
+            try
+            {
+                DataAccess.Update_RethrowingWithExceptionFilter();
+            }
+            catch (DataAccessException dae)
+            {
+                newStackTrace = dae.StackTrace;
+            }
+        }
+    }
+}
---
+++ RoslynUpcomingCSharpNews/IndexedMembers.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace RoslynUpcomingCSharpNews
+{
+    [TestClass]
+    public class IndexedMembers
+    {
+        [TestMethod]
+        public void TestIndexedMembers()
+        {
+            var jsonContent = "{ 'FirstName': 'Max', 'LastName': 'Muster' }";
+            var objectContent = JsonConvert.DeserializeObject(jsonContent) as JObject;
+
+            // Use "old" indexed member syntax.
+            Assert.AreEqual("Max", objectContent["FirstName"]);
+
+            // Use "new" indexed member syntax.
+            Assert.AreEqual("Max", objectContent.$FirstName);
+            objectContent.$Age = 20;
+            Assert.AreEqual(20, objectContent.$Age);
+        }
+
+        private class Person(public string FirstName, public string LastName)
+        { }
+
+        [TestMethod]
+        public void TestElementInitializers()
+        {
+            // C# supports collection initialization for quite a while now.
+            var people = new List<Person>()
+            {
+                new Person("Max", "Muster"),
+                new Person("Tim", "Smith")
+            };
+
+            // It has not been possible for dictionaries.
+            var peopleDictionary = new Dictionary<string, Person>();
+            peopleDictionary.Add("Max", new Person("Max", "Muster"));
+            peopleDictionary.Add("Tim", new Person("Tim", "Smith"));
+
+            // Now we can use element initializers to initialize dictionaries, too.
+            peopleDictionary = new Dictionary<string, Person>
+            {
+                ["Max"] = new Person("Max", "Muster"),
+                ["Tim"] = new Person("Tim", "Smith")
+            };
+        }
+    }
+}
