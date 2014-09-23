e156172a82a8b7dc782dfa7a09936dba73f13c06
rainer@software-architects.at <rainer@software-architects.at>
2014-09-23 11:21:06 +0200

    StyleCop CodeAnalysis Sample

---
+++ StyleCopCodeAnalysis/CodeAnalysis/CodeAnalysis.cs
@@ -0,0 +1,101 @@
+﻿namespace CodeAnalysisDemo
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Data.SqlClient;
+	using System.IO;
+
+	public interface ISwiftItem<T>
+	{
+		string ItemName { get; }
+		List<T> Values { get; }
+	}
+
+	public interface INamedItem
+	{
+		string ItemName { get; }
+	}
+
+	public class SwiftItem<T> : ISwiftItem<T>
+	{
+		public string ItemName
+		{
+			get;
+			set;
+		}
+
+		public List<T> Values
+		{
+			get;
+			set;
+		}
+	}
+
+	public class SwiftFile
+	{
+		private Stream underlying_File;
+		private string fileName;
+		private object header;
+
+		public SwiftFile(string fileName)
+		{
+			this.underlying_File = new FileStream(fileName, FileMode.Open);
+			this.fileName = fileName;
+			this.header = new object();
+		}
+
+		public List<Tuple<string, object>> Settings
+		{
+			get;
+			set;
+		}
+
+		public void AddObject<T>(SwiftItem<T> newObj)
+		{
+			lock (this.header)
+			{
+				var header = string.Format("{0}: {1}", newObj.ItemName, newObj.Values.Count);
+
+				foreach (var item in newObj.Values)
+				{
+					if (item is INamedItem)
+					{
+						var namedItem = item as INamedItem;
+
+						// do something special with namedItem
+					}
+				}
+
+				// do something with newObj
+			}
+		}
+
+		public void CopyFile(string source_file_name)
+		{
+			using (var reader = new StreamReader(new FileStream(source_file_name, FileMode.Open)))
+			{
+				// TODO: copy file here
+			}
+		}
+
+		public void WriteToDatabase(SqlConnection conn, string tenant)
+		{
+			var cmd = conn.CreateCommand();
+			cmd.CommandText = string.Format("INSERT INTO Target ( Tenant, Data ) VALUES ( {0}, @Data )", tenant);
+
+			// Build rest of the command and execute it
+		}
+
+		public void Close()
+		{
+			try
+			{
+				this.underlying_File.Close();
+			}
+			catch
+			{
+				Console.WriteLine("Error");
+			}
+		}
+	}
+}
---
+++ StyleCopCodeAnalysis/StyleCop/Bucket.cs
@@ -0,0 +1,104 @@
+﻿//-------------------------------------------------------
+// <copyright file="Bucket.cs" company="BASTA 2014">
+//     Copyright (c) BASTA 2014 All rights reserved.
+// </copyright>
+//-------------------------------------------------------
+
+namespace StyleCopDemo.Corrected
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics.CodeAnalysis;
+	using System.Linq;
+	using System.Text;
+
+	/// <summary>
+	/// Implements a bucket
+	/// </summary>
+	/// <typeparam name="T">Type of the elements in the bucket</typeparam>
+	public class Bucket<T>
+	{
+		/// <summary>
+		/// Internal helper to store data
+		/// </summary>
+		private Dictionary<string, T> data = new Dictionary<string, T>();
+
+		/// <summary>
+		/// Initializes a new instance of the Bucket class.
+		/// </summary>
+		public Bucket()
+		{
+		}
+
+		[SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", 
+			Justification = "No time to write documentation...")]
+		public Bucket(IEnumerable<Tuple<string, T>> data)
+		{
+			foreach (var item in data)
+			{
+				this.data[item.Item1] = item.Item2;
+			}
+		}
+
+		/// <summary>
+		/// Gets the element at the specified index.
+		/// </summary>
+		/// <param name="index">Index of the element to get.</param>
+		/// <value>Element at the specified index.</value>
+		/// <returns>Element at specified index.</returns>
+		public T this[string index]
+		{
+			get
+			{
+				if (this.data.ContainsKey(index))
+				{
+					return this.data[index];
+				}
+				else
+				{
+					return default(T);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets the length.
+		/// </summary>
+		/// <returns>Length of the dictionary</returns>
+		public int GetLength()
+		{
+			return this.data.Keys.Count;
+		}
+
+		/// <summary>
+		/// Returns a <see cref="System.String"/> that represents this instance.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="System.String"/> that represents this instance.
+		/// </returns>
+		public override string ToString()
+		{
+			return this.Dump();
+		}
+
+		/// <summary>
+		/// Dumps this instance.
+		/// </summary>
+		/// <returns>String representation of the dictionary</returns>
+		private string Dump()
+		{
+			return this.data.Aggregate<KeyValuePair<string, T>, StringBuilder>(
+			 new StringBuilder(),
+			 (agg, item) =>
+			 {
+				 if (agg.Length > 0)
+				 {
+					 agg.Append(", ");
+				 }
+
+				 agg.AppendFormat("{0}: {1}", item.Key, item.Value.ToString());
+				 return agg;
+			 }).ToString();
+		}
+	}
+}
