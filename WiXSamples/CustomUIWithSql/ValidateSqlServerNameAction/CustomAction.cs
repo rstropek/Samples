50b48d5f0b911f53061475accf5719b7407e8fef
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2014-12-02 10:47:47 +0100

    Custom Action and Custom WiX UI

---
+++ WiXSamples/CustomUIWithSql/ValidateSqlServerNameAction/CustomAction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+using System.Data.SqlClient;
+
+namespace ValidateSqlServerNameAction
+{
+	public class CustomActions
+	{
+		[CustomAction]
+		public static ActionResult ValidateSqlServerName(Session session)
+		{
+			var serverName = session["SQLSERVERNAME"];
+
+			try
+			{
+				using (var conn = new SqlConnection(string.Format("Server={0};Database=master;Integrated Security=true;Connection Timeout=10", serverName)))
+				{
+					conn.Open();
+				}
+
+				session["SQLEXCEPTION"] = string.Empty;
+			}
+			catch (SqlException ex)
+			{
+				session["SQLEXCEPTION"] = ex.Message;
+			}
+
+			return ActionResult.Success;
+		}
+	}
+}
