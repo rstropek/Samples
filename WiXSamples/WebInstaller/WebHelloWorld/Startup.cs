937b9cb4ec063f10eafd2b624ea31248858309ca
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2015-01-13 00:55:27 +0100

    Added sample for installing an OWIN web application

---
+++ WiXSamples/WebInstaller/WebHelloWorld/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebHelloWorld.Startup))]
+
+namespace WebHelloWorld
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.Run(context => context.Response.WriteAsync("<html><body><h1>Hello World!</h1></body></html>"));
+		}
+	}
+}
