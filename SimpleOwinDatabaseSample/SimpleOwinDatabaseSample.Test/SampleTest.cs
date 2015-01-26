ad0c571a42f09baf281c580850aa307246a4583c
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2015-01-26 20:38:11 +0100

    Added sample

---
+++ SimpleOwinDatabaseSample/SimpleOwinDatabaseSample.Test/SampleTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SimpleOwinDatabaseSample.Test
+{
+	[TestClass]
+	public class SampleTest
+	{
+		public async Task RestoreTestDBAsync()
+		{
+			// In practice you would probably create a test database using DACPAC/BACPAC,
+			// RESTORE, T-SQL script, etc. Here we create the test DB with two
+			// sample tables just for demo purposes.
+
+			var connectionString = ConfigurationManager.ConnectionStrings["SampleDBServer"].ConnectionString;
+			var masterConnectionString = Regex.Replace(connectionString, "Database=[^;]+;", "Database=master;");
+			using (var conn = new SqlConnection(masterConnectionString))
+			{
+				await conn.OpenAsync();
+				using (var cmd = conn.CreateCommand())
+				{
+					cmd.CommandText = "SELECT MAX(database_id) FROM sys.databases WHERE name = 'OOP_ALM_Talk'";
+					if (await cmd.ExecuteScalarAsync() == System.DBNull.Value)
+					{
+						cmd.CommandText = "CREATE DATABASE OOP_ALM_Talk";
+						await cmd.ExecuteNonQueryAsync();
+
+						cmd.CommandText = @"
+							CREATE TABLE OOP_ALM_Talk.dbo.Dev ( ID INT PRIMARY KEY );
+							CREATE TABLE OOP_ALM_Talk.dbo.Server ( ID INT PRIMARY KEY );";
+						await cmd.ExecuteNonQueryAsync();
+					}
+				}
+			}
+		}
+
+		[TestMethod]
+		public void DummyTest()
+		{
+			// This is just a dummy test. If you want to check whether build fails
+			// in case of a failing test, simply change this dummy test.
+			Assert.IsTrue(1 == 1);
+		}
+
+		[TestMethod]
+		public async Task DatabaseTest()
+		{
+			await RestoreTestDBAsync();
+
+			// Note that this test really accesses a database. So we need a database
+			// (localdb) on the build server.
+
+			var pt = new PrivateType(typeof(SimpleOwinDatabaseSample.Startup));
+			var tables = await (pt.InvokeStatic("GetTableNamesAsync") as Task<IEnumerable<string>>);
+			Assert.IsNotNull(tables);
+			Assert.IsTrue(tables.Count() > 0);
+		}
+	}
+}
---
+++ SimpleOwinDatabaseSample/SimpleOwinDatabaseSample/Startup.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SimpleOwinDatabaseSample.Startup))]
+
+namespace SimpleOwinDatabaseSample
+{
+	public class Startup
+	{
+		#region HTML Fragments
+		private const string HtmlHeader = @"<!doctype html>
+<html>
+<body>
+	<h1>Simple OWIN Database Sample</h1>
+";
+		private const string ServerInfoHeader = @"	<h2>Server</h2>
+	<p>
+";
+		private const string ServerInfoFooter = @"	</p>
+";
+		private const string TableListHeader = @"	<h2>List of Tables</h2>
+	<ul>
+";
+		private const string TableListFooter = @"	</ul>
+";
+		private const string HtmlFooter = @"</body>
+</html>";
+		#endregion
+
+		public void Configuration(IAppBuilder app)
+		{
+			app.Run(async context =>
+			{
+				if (context.Request.Method != "GET")
+				{
+					context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+				}
+
+				await context.Response.WriteAsync(HtmlHeader);
+
+				await context.Response.WriteAsync(ServerInfoHeader);
+				await context.Response.WriteAsync(string.Format("	Local IP: {0}<br/>\n", context.Request.LocalIpAddress));
+				await context.Response.WriteAsync(string.Format("	Local Port: {0}<br/>\n", context.Request.LocalPort));
+				await context.Response.WriteAsync(string.Format("	Remote IP: {0}<br/>\n", context.Request.RemoteIpAddress));
+				await context.Response.WriteAsync(string.Format("	Remote Port: {0}<br/>\n", context.Request.RemotePort));
+				await context.Response.WriteAsync(ServerInfoFooter);
+	
+				await context.Response.WriteAsync(TableListHeader);
+
+				foreach (var tableName in await GetTableNamesAsync())
+				{
+					await context.Response.WriteAsync(string.Format("		<li>{0}</li>\n", tableName));
+				}
+
+				await context.Response.WriteAsync(TableListFooter);
+				await context.Response.WriteAsync(HtmlFooter);
+			});
+		}
+
+		private static async Task<IEnumerable<string>> GetTableNamesAsync()
+		{
+			var result = new List<string>();
+			var connectionString = ConfigurationManager.ConnectionStrings["SampleDBServer"].ConnectionString;
+			using (var conn = new SqlConnection(connectionString))
+			{
+				await conn.OpenAsync();
+				using (var cmd = conn.CreateCommand())
+				{
+					cmd.CommandText = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES";
+					using (var reader = await cmd.ExecuteReaderAsync())
+					{
+						while (await reader.ReadAsync())
+						{
+							result.Add(reader.GetString(0));
+						}
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
