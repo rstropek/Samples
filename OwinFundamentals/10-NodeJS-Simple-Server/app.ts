cc60a131f91a24a06381141296af7021afecf75f
Rainer Stropek <rainer@timecockpit.com>
2015-05-04 18:19:35 +0200

    Added node intro samples

---
+++ OwinFundamentals/10-NodeJS-Simple-Server/app.ts
@@ -0,0 +1,14 @@
+﻿import http = require("http");
+
+var server = http.createServer((req, resp) => {
+    console.log("Got request for " + req.url);
+
+    resp.writeHead(200, { "Content-Type": "text/html" });
+    resp.write('<!DOCTYPE "html"><html><body><h1>Hello from ');
+    resp.write(req.url);
+    resp.write('!</h1></body></html>');
+    resp.end();
+});
+
+server.listen(1337);
+console.log("Listening on port 1337 ...");
---
+++ OwinFundamentals/15-NodeJS-Edge/app.ts
@@ -0,0 +1,17 @@
+﻿import express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/customer/:id',(req, res) => {
+    var customer = {
+        customerId: req.params.id,
+        customerName: "Customer " + req.params.id
+    };
+    res.status(200).send(customer);
+});
+
+var server = app.listen(
+    1337,() => console.log("Listening on port 1337 ..."));
