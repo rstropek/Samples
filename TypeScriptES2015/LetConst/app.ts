1169b484efc1f71096c31fb4cf29461816799e5b
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2015-07-21 12:49:41 +0200

    const and let

---
+++ TypeScriptES2015/LetConst/app.ts
@@ -0,0 +1,33 @@
+function doSomething(num: number) {
+	if (num % 2 == 0){
+		// Note that 'isEven' is function-scoped. Its scope is
+		// not limited to the 'if' statement. If you replace 'var'
+		// with 'let', you will get a syntax error as the scope
+		// of 'isEven' changes to the current block. 
+		var isEven = true;
+	}
+	else {
+		isEven = false;
+	}
+	
+	return isEven;
+}
+
+function doSomethingWithLet(num: number) {
+	// Note that we have to define 'isEven' here to make
+	// it function-scoped.
+	let isEven: boolean;
+	if (num % 2 == 0){
+		// If you add 'let' here, this 'isEven' would refer
+		// to a different variable than the 'isEven' shown above. 
+		isEven = true;
+	}
+	else {
+		isEven = false;
+	}
+	
+	return isEven;
+}
+
+console.log(doSomething(2));
+console.log(doSomethingWithLet(2));
