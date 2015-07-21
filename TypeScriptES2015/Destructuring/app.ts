c8e38366f7959542d9b4de44d01ee5bd70c13a2c
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2015-07-21 08:16:56 +0200

    Destructuring

---
+++ TypeScriptES2015/Destructuring/app.ts
@@ -0,0 +1,32 @@
+/// <reference path="typings/tsd.d.ts" />
+
+interface IPerson {
+	firstName: string;
+	lastName: string;
+}
+
+class DemoController {
+	public person: IPerson = { firstName: "", lastName: "" };
+	public personList: IPerson[] = [ 
+		{ firstName: "Tom", lastName: "Turbo" },
+		{ firstName: "Max", lastName: "Muster" } ];
+	
+	getFullName() {
+		// Note destructuring of person object in local variables
+		// firstName and lastName
+		var { firstName, lastName } = this.person;
+		
+		// Note using the new template string 
+		return `${firstName} ${lastName}`;
+	}
+	
+	getColleagues() {
+		// Note destructuring of an array in local variables p1 and p2
+		var [ p1, p2 ] = this.personList;
+
+		// Note using the new template string 
+		return `Your colleagues are ${p1.firstName} and ${p2.firstName}`;
+	}
+}
+
+angular.module("demo", []).controller("demo", DemoController);
