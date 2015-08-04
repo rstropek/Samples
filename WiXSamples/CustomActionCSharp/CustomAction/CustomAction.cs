d32541570b85c0be5a72bf210b0f5ac2b9491505
rainer@software-architects.at <4GO1dWmzP3izDvDArcq3>
2015-08-04 12:16:54 +0200

    Added custom action sample (C#)

---
+++ WiXSamples/CustomActionCSharp/CustomAction/CustomAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace CustomAction
+{
+	public class CustomActions
+	{
+		[CustomAction]
+		public static ActionResult GetRandomNumber(Session session)
+		{
+			session["RANDOM_NUMBER"] = new Random().Next(100000).ToString();
+			return ActionResult.Success;
+		}
+	}
+}
