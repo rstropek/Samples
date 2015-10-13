4206fe573f91a48020726d434b2f4921497c03ae
Rainer Stropek <rainer@timecockpit.com>
2015-10-13 07:44:30 +0200

    Add simple console app for CPU profiling basics

---
+++ ProfilingWorkshop/SimpleCpuProfiling/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleCpuProfiling
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HavingFunWithDates();
+			HavingFunWithSleeping();
+		}
+
+		static void HavingFunWithDates()
+		{
+			for (var i = 0; i < 5000; i++)
+			{
+				GetSomeDates();
+				GetSomeUTCDates();
+			}
+		}
+
+		static void HavingFunWithSleeping()
+		{
+			Sleep();
+			BurnCpuCycles();
+		}
+
+		static void GetSomeDates()
+		{
+			for(var i=0; i<1000; i++)
+			{
+				var dt = DateTime.Now;
+			}
+		}
+
+		static void GetSomeUTCDates()
+		{
+			for (var i = 0; i < 1000; i++)
+			{
+				var dt = DateTime.UtcNow;
+			}
+		}
+
+		static void Sleep()
+		{
+			for (var i=0; i<1000; i++)
+			{
+				Thread.Sleep(1);
+			}
+		}
+
+		static void BurnCpuCycles()
+		{
+			for (var i = 0; i<1000; i++)
+			{
+				Thread.SpinWait(500000);
+			}
+		}
+	}
+}
