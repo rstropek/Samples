b654d6ffe6c7d99db731eea079916d3d849c43ae
Rainer Stropek <rainer@timecockpit.com>
2015-11-23 10:52:35 +0100

    Add PaaS ARM Sample

---
+++ ArmWorkshop/04-PaaS-Web-DB/deploy.ps1
@@ -0,0 +1,5 @@
+﻿$rg = "RG-PaaS-Web-DB"
+$dep = [guid]::NewGuid()
+$path = "C:\Code\Github\Samples\ArmWorkshop\04-PaaS-Web-DB"
+New-AzureRmResourceGroup -Name $rg -Location "northeurope"
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$path\azuredeploy.json" -TemplateParameterFile "$path\azuredeploy-parameters.json" -Name $dep
