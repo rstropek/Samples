be5d54ab35cffde1f8c72ecb82368cacf5608a17
Rainer Stropek <rainer@timecockpit.com>
2016-02-29 07:47:29 +0100

    Add parallel test execution sample

---
+++ ParallelTestExecution/TestProject2/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestCategory("TestProject2")]
+        [TestMethod]
+        public async Task TestMethod1() => await this.DummyTest();
+
+        [TestCategory("TestProject2")]
+        [TestMethod]
+        public async Task TestMethod2() => await this.DummyTest();
+
+        [TestCategory("TestProject2")]
+        [TestMethod]
+        public async Task TestMethod3() => await this.DummyTest();
+
+        [TestCategory("TestProject2")]
+        [TestMethod]
+        public async Task TestMethod4() => await this.DummyTest();
+
+        private async Task DummyTest([CallerMemberName] string caller = null)
+        {
+            Debug.WriteLine($"Starting {caller} ...");
+            await Task.Delay(1000);
+            Assert.AreEqual(1, 1);
+            Debug.WriteLine($"Finished {caller} ...");
+        }
+    }
+}
