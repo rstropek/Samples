567acd8a5b7a194e55cbe15c5518bfd88ad14f28
Rainer Stropek <rainer@software-architects.at>
2016-06-15 10:21:33 +0200

    Refresh ARM samples

--- ArmWorkshop/02-Docker/deploy.ps1
+++ ArmWorkshop/02-Docker/deploy.ps1
@@ -4,7 +4,7 @@ Get-AzureRmSubscription | where { $_.SubscriptionName -like "*Sponsorship*" } | 
 $rg = "RG-Docker"
 $dep = "Simple-Docker-Deployment"
 $path = "C:\Code\Github\Samples\ArmWorkshop\02-Docker"
-New-AzureRmResourceGroup -Name $rg -Location "northeurope"
+New-AzureRmResourceGroup -Name $rg -Location "northeurope" -Force
 New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$path\azuredeploy.json" `
     -TemplateParameterFile "$path\azuredeploy.parameters.json" -Name $dep
 
--- ArmWorkshop/04-PaaS-Web-DB/deploy.ps1
+++ ArmWorkshop/04-PaaS-Web-DB/deploy.ps1
@@ -1,5 +1,15 @@
-﻿$rg = "RG-PaaS-Web-DB"
-$dep = [guid]::NewGuid()
+﻿# Login-AzureRmAccount
+Get-AzureRmSubscription | where { $_.SubscriptionName -like "*Sponsorship*" } | Select-AzureRmSubscription
+
+$rg = "RG-PaaS-Web-DB"
+$dep = "PaaS-Deployment-" + [guid]::NewGuid()
 $path = "C:\Code\Github\Samples\ArmWorkshop\04-PaaS-Web-DB"
-New-AzureRmResourceGroup -Name $rg -Location "northeurope"
-New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$path\azuredeploy.json" -TemplateParameterFile "$path\azuredeploy-parameters.json" -Name $dep
+
+$group = Get-AzureRmResourceGroup -Name $rg -ErrorAction SilentlyContinue
+if (!$group) {
+    New-AzureRmResourceGroup -Name $rg -Location "northeurope"
+}
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$path\azuredeploy.json" `    -TemplateParameterFile "$path\azuredeploy-parameters.json" -Name $dep
+
+# Get-AzureRmResourceGroup -Name $rg | Remove-AzureRmResourceGroup -Force
