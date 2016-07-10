3657ac495b5f73cc1756c88655fc1490cd8c3f6f
Rainer Stropek <rainer@software-architects.at>
2016-07-10 16:00:41 +0200

    Add presentation and samples (.NET Core, ASP.NET Core, Roslyn) for C# Design Patterns Workshop

---
+++ CSharpDesignPatterns/ObserverInDiagnostics/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+namespace ObserverInDiagnostics
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            new WebHostBuilder().UseKestrel().UseStartup<Startup>().Build().Run();
+    }
+
+    public class Startup : IObserver<KeyValuePair<string, object>>
+    {
+        private readonly byte[] message = Encoding.UTF8.GetBytes("Hello World");
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Note how the Observable pattern is used in DiagnosticListener. It implements
+            // IObservable<T>, therefore we can subscribe to events.
+            var listener = new DiagnosticListener("Microsoft.AspNetCore");
+            listener.Subscribe(this);
+
+            services.AddSingleton<DiagnosticSource>(listener);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Map("/error", errorApp =>
+                // Throw exception for demo purposes
+                errorApp.Run(h => { throw new DivideByZeroException(); }));
+
+            app.Run(h => h.Response.Body.WriteAsync(message, 0, message.Length));
+        }
+
+        #region Implementation of IObserver<T>
+        public void OnCompleted() => Console.WriteLine("OnCompleted");
+        public void OnError(Exception error) { throw error; }
+        public void OnNext(KeyValuePair<string, object> value) => Console.WriteLine($"OnNext: {value.Key}");
+        #endregion
+    }
+}
---
+++ CSharpDesignPatterns/RoslynVisitor/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+
+namespace RoslynVisitor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var tree = CSharpSyntaxTree.ParseText(
+                @"class Test { public void DoSomething() => 
+                    System.Console.WriteLine(""Hello World!""); }");
+
+            var v = new PrintMethodNamesVisitor();
+            v.Visit(tree.GetRoot());
+
+            var r = new MethodRenamingRewriter();
+            var newRoot = r.Visit(tree.GetRoot());
+            Console.WriteLine(newRoot.ToString());
+            v.Visit(newRoot);
+        }
+    }
+
+    public class PrintMethodNamesVisitor : CSharpSyntaxWalker
+    {
+        public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            Console.WriteLine(node.Identifier.Value);
+            base.VisitMethodDeclaration(node);
+        }
+    }
+
+    public class MethodRenamingRewriter : CSharpSyntaxRewriter
+    {
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            if (node.Identifier.Value.ToString() == "DoSomething")
+            {
+                return node.ReplaceNode(node, SyntaxFactory.MethodDeclaration(
+                    node.AttributeLists,
+                    node.Modifiers,
+                    node.ReturnType,
+                    node.ExplicitInterfaceSpecifier,
+                    SyntaxFactory.Identifier("DoSomethingElse"),
+                    node.TypeParameterList,
+                    node.ParameterList,
+                    node.ConstraintClauses,
+                    node.Body,
+                    node.ExpressionBody));
+            }
+
+            return node;
+        }
+    }
+}
