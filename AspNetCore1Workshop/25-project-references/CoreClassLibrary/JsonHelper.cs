7b24fd9b7ff025abc5a19f3835d6e2e3bcbd66e1
Rainer Stropek <rainer@timecockpit.com>
2017-02-14 20:48:58 +0100

    Add .NET Core solution samples

---
+++ AspNetCore1Workshop/25-project-references/CoreClassLibrary/JsonHelper.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CoreClassLibrary
+{
+    public static class JsonHelper
+    {
+        public static JObject Parse(string content)
+        {
+            return (JObject)JsonConvert.DeserializeObject(content);
+        }
+    }
+}
---
+++ AspNetCore1Workshop/25-project-references/FullFrameworkClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using CoreClassLibrary;
+using System;
+
+namespace FullFrameworkClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string content = @"{ ""name"": ""Superman"" }";
+            var result = JsonHelper.Parse(content);
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
