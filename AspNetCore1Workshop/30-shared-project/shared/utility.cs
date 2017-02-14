0c0780c9a421f8866fc7c33826c74f33a0943241
Rainer Stropek <rainer@timecockpit.com>
2017-02-14 21:16:56 +0100

    Remove demo for shared project

--- AspNetCore1Workshop/30-shared-project/shared/utility.cs
+++
@@ -1,8 +0,0 @@
-namespace Utilities
-{
-    // Note that this class is internal as the code is shared
-    internal static class Utility
-    {
-        public static string Greeting { get; } = "Hello World!";
-    }
-}
