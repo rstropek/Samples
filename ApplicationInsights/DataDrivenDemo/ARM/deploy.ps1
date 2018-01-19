69a4b380194fc947581f3b7285d10a12eed3192f
Rainer Stropek <rainer@software-architects.at>
2018-01-19 17:57:13 +0100

    ARM templates

---
+++ ApplicationInsights/DataDrivenDemo/ARM/deploy.ps1
@@ -0,0 +1,28 @@
+﻿# Check if user is already signed in
+$context = Get-AzureRmContext
+if (!$context.Subscription.Name) {
+    Login-AzureRmAccount
+}
+
+# Select subscription where name contains `MSDN Subscription`
+Get-AzureRmSubscription | where { $_.Name -like "*MSDN Subscription*" } | Select-AzureRmSubscription
+
+# Set some string constants
+$rg = "data-driven-app-insights"
+$location = "westeurope"
+$dep = "Deployment-" + [guid]::NewGuid()
+$dbAdminUser = "demo"
+
+# Check if resource group already exists
+$group = Get-AzureRmResourceGroup -Name $rg -ErrorAction SilentlyContinue
+if (!$group) {
+    New-AzureRmResourceGroup -Name $rg -Location $location
+}
+
+if (!$cred) {
+    $cred = Get-Credential
+}
+
+# Deploy ARM template
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile "$PSScriptRoot\main.json" `
+    -Name $dep -dbAdminUser $dbAdminUser -dbAdminPassword $cred.Password
