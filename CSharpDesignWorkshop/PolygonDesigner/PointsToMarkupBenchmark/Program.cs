cd1f1c69698fc6264bf43ce052362e3f6e33a7ef
Rainer Stropek <rainer@software-architects.at>
2019-09-23 08:39:28 +0200

    Add Benchmark to Sample

---
+++ CSharpDesignWorkshop/PolygonDesigner/PointsToMarkupBenchmark/Program.cs
@@ -0,0 +1,92 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+using Polygon.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointsToMarkupBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MarkupToPoints>();
+            BenchmarkRunner.Run<PointsToMarkup>();
+        }
+    }
+
+    [SimpleJob(RunStrategy.ColdStart, targetCount: 5, invocationCount: 1000)]
+    [MemoryDiagnoser]
+    public class MarkupToPoints
+    {
+        [ParamsSource(nameof(Markups))]
+        public string PathMarkup { get; set; }
+
+        public IEnumerable<string> Markups { get; set; }
+
+        public MarkupToPoints()
+        {
+            var markups = new[]
+            {
+                "M0,0Z",
+                "M0,0L10,0L10,10L0,10Z",
+                ""
+            };
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < 1000; i++)
+            {
+                sb.Append(i == 0 ? "M" : "L");
+                sb.Append($"{i},{i}");
+            }
+            sb.Append("Z");
+            markups[2] = sb.ToString();
+            Markups = markups;
+        }
+
+        [Benchmark]
+        public void StringToPoints() => PathMarkupConverter.Convert(PathMarkup);
+
+        [Benchmark]
+        public void StringToPointsOld() => PathMarkupConverter.ConvertOld(PathMarkup);
+    }
+
+
+    [SimpleJob(RunStrategy.ColdStart, targetCount: 5, invocationCount: 1000)]
+    [MemoryDiagnoser]
+    public class PointsToMarkup
+    {
+        [ParamsSource(nameof(PointsData))]
+        public ReadOnlyMemory<Point> Points { get; set; }
+
+        public IEnumerable<ReadOnlyMemory<Point>> PointsData { get; set; }
+
+        public PointsToMarkup()
+        {
+            var points = new[]
+            {
+                PathMarkupConverter.Convert("M0,0Z"),
+                PathMarkupConverter.Convert("M0,0L10,0L10,10L0,10Z"),
+                null
+            };
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < 1000; i++)
+            {
+                sb.Append(i == 0 ? "M" : "L");
+                sb.Append($"{i},{i}");
+            }
+            sb.Append("Z");
+            points[2] = PathMarkupConverter.Convert(sb.ToString());
+            PointsData = points;
+        }
+
+        [Benchmark]
+        public void PointsToString() => PathMarkupConverter.Convert(Points);
+
+        [Benchmark]
+        public void PointsToStringOld() => PathMarkupConverter.ConvertOld(Points.Span);
+    }
+}
--- CSharpDesignWorkshop/PolygonDesigner/Polygon.Core/PointsToPathMarkup.cs
+++ CSharpDesignWorkshop/PolygonDesigner/Polygon.Core/PointsToPathMarkup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Polygon.Core
@@ -47,6 +49,25 @@ namespace Polygon.Core
             });
         }
 
+        public static string ConvertOld(ReadOnlySpan<Point> points)
+        {
+            var builder = new StringBuilder(points.Length * 20);
+            void AppendDouble(in double d) => builder.Append(Math.Round(d, 4).ToString("G", CultureInfo.InvariantCulture));
+
+            for (var i = 0; i < points.Length; i++)
+            {
+                builder.Append(i == 0 ? 'M' : 'L');
+                var p = points[i];
+                AppendDouble(p.X);
+                builder.Append(',');
+                AppendDouble(p.Y);
+            }
+
+            builder.Append('Z');
+
+            return builder.ToString();
+        }
+
         public static ReadOnlyMemory<Point> Convert(string pointsMarkup)
         {
             if (string.IsNullOrEmpty(pointsMarkup))
@@ -84,6 +105,37 @@ namespace Polygon.Core
 
             // Note that we can return an array as memory
             return result;
+        }
+
+        public static ReadOnlyMemory<Point> ConvertOld(string pointsMarkup)
+        {
+            if (string.IsNullOrEmpty(pointsMarkup))
+            {
+                return Array.Empty<Point>();
+            }
+
+            if (!Regex.IsMatch(pointsMarkup, @"^M[-+]?\d+(\.\d+)?,[-+]?\d+(\.\d+)?(L[-+]?\d+(\.\d+)?,[-+]?\d+(\.\d+)?)*Z$"))
+            {
+                throw new ArgumentException("Point markup is invalid", nameof(pointsMarkup));
+            }
+
+            // Note that we cannot use span'ified APIs here because the parse methods
+            // are not available in .NET Standard 2.0. They will be in 2.1.
+
+            var result = new List<Point>();
+            var index = 0;
+            while (pointsMarkup[index] != 'Z')
+            {
+                var indexOfComma = pointsMarkup.IndexOf(',', index);
+                var indexOfNextLine = pointsMarkup.IndexOfAny(new[] { 'L', 'Z' }, indexOfComma);
+                var x = double.Parse(pointsMarkup.Substring(index + 1, indexOfComma - index - 1));
+                var y = double.Parse(pointsMarkup.Substring(indexOfComma + 1, indexOfNextLine - indexOfComma - 1));
+                result.Add(new Point(x, y));
+
+                index = indexOfNextLine;
+            }
+
+            return result.ToArray();
         }
     }
 }
