ed1936e64d817c48acad040a93c08a834081bae5
Rainer Stropek <rainer@software-architects.at>
2020-08-20 07:37:39 +0200

    Update dependencies for Grpc and SignalR samples

--- BeyondREST/BeyondREST/GrpcClient/Program.cs
+++ BeyondREST/BeyondREST/GrpcClient/Program.cs
@@ -4,97 +4,89 @@ using GrpcDemo;
 using System;
 using System.Threading.Tasks;
 
-namespace GrpcClient
+AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+// Create channel.
+// Represents long-lived connection to gRPC service.
+// The port number(5001) must match the port of the gRPC server.
+// Tip: In ASP.NET Core apps, use client factory (similar to
+//      IHttpClientFactory (see https://docs.microsoft.com/en-us/aspnet/core/grpc/clientfactory).
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+await UnaryCall(channel);
+
+// Tip: Reuse channel for gRPC calls, create multiple gRPC clients
+//      from it (including different types of clients).
+var mathClient = new MathGuru.MathGuruClient(channel);
+await ServerStreaming(mathClient);
+
+await BidirectionalStreaming(mathClient);
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+
+static async Task UnaryCall(GrpcChannel channel)
 {
-    class Program
+    var client = new Greeter.GreeterClient(channel);
+    var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+    Console.WriteLine("Greeting: " + reply.Message);
+}
+
+static async Task ServerStreaming(MathGuru.MathGuruClient mathClient)
+{
+    // Note usage of new using statement (C# 8)
+    using var fibResult = mathClient.GetFibonacci(new FromTo() { From = 10, To = 50 });
+
+    // Note usage of new streaming enumerable
+    await foreach (var fib in fibResult.ResponseStream.ReadAllAsync())
     {
-        static async Task Main()
+        Console.WriteLine(fib);
+    }
+}
+
+static async Task BidirectionalStreaming(MathGuru.MathGuruClient mathClient)
+{
+    using var duplexStream = mathClient.GetFibonacciStepByStep();
+
+    // Note that client processes streamed results from server asynchronously
+    var receiverTask = Task.Run(async () =>
+    {
+        // Note that this task will end when the server has closed the response stream
+        await foreach (var fib in duplexStream.ResponseStream.ReadAllAsync())
         {
-            AppContext.SetSwitch(
-                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-
-            // Create channel.
-            // Represents long-lived connection to gRPC service.
-            // The port number(5001) must match the port of the gRPC server.
-            // Tip: In ASP.NET Core apps, use client factory (similar to
-            //      IHttpClientFactory (see https://docs.microsoft.com/en-us/aspnet/core/grpc/clientfactory).
-            var channel = GrpcChannel.ForAddress("http://localhost:8080");
-
-            await UnaryCall(channel);
-
-            // Tip: Reuse channel for gRPC calls, create multiple gRPC clients
-            //      from it (including different types of clients).
-            var mathClient = new MathGuru.MathGuruClient(channel);
-            await ServerStreaming(mathClient);
-
-            await BidirectionalStreaming(mathClient);
-
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-        }
-
-        private static async Task UnaryCall(GrpcChannel channel)
-        {
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
-            Console.WriteLine("Greeting: " + reply.Message);
-        }
-
-        private static async Task ServerStreaming(MathGuru.MathGuruClient mathClient)
-        {
-            // Note usage of new using statement (C# 8)
-            using var fibResult = mathClient.GetFibonacci(new FromTo() { From = 10, To = 50 });
-
-            // Note usage of new streaming enumerable
-            await foreach (var fib in fibResult.ResponseStream.ReadAllAsync())
+            switch (fib.PayloadCase)
             {
-                Console.WriteLine(fib);
+                case StepByStepResult.PayloadOneofCase.Error:
+                    Console.Error.WriteLine($"ERROR: {fib.Error.Message}");
+                    break;
+                case StepByStepResult.PayloadOneofCase.Result:
+                    Console.WriteLine(fib.Result.Result);
+                    if (fib.Result.Result == 34)
+                    {
+                        // Ok, we got the highest Fibonacci number we were looking for.
+                        // We can close the request stream so the server can gracefully stop.
+                        await duplexStream.RequestStream.CompleteAsync();
+                    }
+                    break;
+                default:
+                    // This should never happen
+                    throw new InvalidOperationException($"Unexpected payload {fib.PayloadCase}");
             }
         }
+    });
 
-        private static async Task BidirectionalStreaming(MathGuru.MathGuruClient mathClient)
-        {
-            using var duplexStream = mathClient.GetFibonacciStepByStep();
+    // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+    await duplexStream.RequestStream.WriteAsync(new FromTo() { From = 1, To = 10 });
 
-            // Note that client processes streamed results from server asynchronously
-            var receiverTask = Task.Run(async () =>
-            {
-                // Note that this task will end when the server has closed the response stream
-                await foreach (var fib in duplexStream.ResponseStream.ReadAllAsync())
-                {
-                    switch (fib.PayloadCase)
-                    {
-                        case StepByStepResult.PayloadOneofCase.Error:
-                            Console.Error.WriteLine($"ERROR: {fib.Error.Message}");
-                            break;
-                        case StepByStepResult.PayloadOneofCase.Result:
-                            Console.WriteLine(fib.Result.Result);
-                            if (fib.Result.Result == 34)
-                            {
-                                // Ok, we got the highest Fibonacci number we were looking for.
-                                // We can close the request stream so the server can gracefully stop.
-                                await duplexStream.RequestStream.CompleteAsync();
-                            }
-                            break;
-                        default:
-                            // This should never happen
-                            throw new InvalidOperationException($"Unexpected payload {fib.PayloadCase}");
-                    }
-                }
-            });
+    // Note that the following request will result in an error
+    // because From (5) is < previous To (10)
+    await duplexStream.RequestStream.WriteAsync(new FromTo() { From = 5, To = 15 });
 
-            // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
-            await duplexStream.RequestStream.WriteAsync(new FromTo() { From = 1, To = 10 });
+    // Request a meaningful result
+    await duplexStream.RequestStream.WriteAsync(new FromTo() { From = 20, To = 40 });
 
-            // Note that the following request will result in an error
-            // because From (5) is < previous To (10)
-            await duplexStream.RequestStream.WriteAsync(new FromTo() { From = 5, To = 15 });
-
-            // Request a meaningful result
-            await duplexStream.RequestStream.WriteAsync(new FromTo() { From = 20, To = 40 });
-
-            // Wait until the async processing task has ended
-            await receiverTask;
-        }
-    }
+    // Wait until the async processing task has ended
+    await receiverTask;
 }
--- BeyondREST/BeyondREST/GrpcServer/Program.cs
+++ BeyondREST/BeyondREST/GrpcServer/Program.cs
@@ -1,27 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using GrpcServer.Services;
+using GrpcServer;
 
-namespace GrpcServer
+Host.CreateDefaultBuilder(args)
+.ConfigureWebHostDefaults(webBuilder =>
 {
-    public class Program
+    webBuilder.ConfigureServices(services =>
     {
-        public static void Main(string[] args)
+        services.AddSingleton<MathAlgorithms>();
+        services.AddGrpc();
+
+        services.AddCors(o => o.AddPolicy("AllowAll", builder =>
         {
-            CreateHostBuilder(args).Build().Run();
+            builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithExposedHeaders("Grpc-Status", "Grpc-Message");
+        }));
+    })
+    .Configure((context, app) =>
+    {
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+        app.UseRouting();
+
+        app.UseGrpcWeb();
+        app.UseCors();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb().RequireCors("AllowAll");
+            endpoints.MapGrpcService<MathGuruService>().EnableGrpcWeb().RequireCors("AllowAll");
+
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+            });
+        });
+    });
+})
+.Build()
+.Run();
