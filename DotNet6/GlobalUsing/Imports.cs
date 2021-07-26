fd23b1b97e4e25186ebb262c93e611fe924de496
Rainer Stropek <rainer@software-architects.at>
2021-07-26 08:40:31 +0200

    Add Global Using sample

---
+++ DotNet6/GlobalUsing/Imports.cs
@@ -0,0 +1,4 @@
+﻿global using System;
+global using static System.Console;
+global using System.Linq;
+global using System.Text;
---
+++ DotNet6/GlobalUsing/Program.cs
@@ -0,0 +1,14 @@
+﻿// Note: no `using System` necessary
+var dates = new DateOnly[] { new(2021, 1, 1), new(2022, 1, 1) };
+
+// Note: no `using System.Text` necessary
+var builder = new StringBuilder();
+
+// Note: no `using System.Linq` necessary
+var datesString = dates.Aggregate(
+    new StringBuilder(),
+    (sb, d) => sb.AppendLine(d.ToString("o")),
+    sb => sb.ToString());
+
+// Note: no `Console` necessary
+WriteLine(datesString);
