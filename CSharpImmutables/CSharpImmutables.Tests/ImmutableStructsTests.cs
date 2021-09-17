08dd5f1b1221e8ea537fdfe7779f28040227456b
Rainer Stropek <rainer@software-architects.at>
2021-09-17 08:51:30 +0200

    Add immutable structs samples (WIP)

--- CSharpImmutables/CSharpImmutables.Tests/ImmutableStructsTests.cs
+++ CSharpImmutables/CSharpImmutables.Tests/ImmutableStructsTests.cs
@@ -8,17 +8,22 @@ namespace CSharpImmutables.Tests
             var v = new MutableVector2d();
             Assert.Equal(new(0d, 0d), v);
 
-            v = new MutableVector2d(1d, 1d);
+            v = new(1d, 1d);
             Assert.Equal(new(1d, 1d), v);
 
             v = new MutableVector2d { X = 1d, Y = 1d, };
             Assert.Equal(new(1d, 1d), v);
 
-            v = new MutableVector2d(1d, 1d);
+            v = new(1d, 1d);
             v.Double();
             Assert.Equal(new(2d, 2d), v);
 
-            Assert.Equal(Math.Sqrt(2d), new MutableVector2d(1d, 1d).Distance);
+            v = new(1d, 1d);
+            v.Move(2d, 2d);
+            Assert.Equal(new(3d, 3d), v);
+
+            v = new(1d, 1d);
+            Assert.Equal(Math.Sqrt(2d), v.Distance);
         }
 
         [Fact]
@@ -27,17 +32,18 @@ namespace CSharpImmutables.Tests
             var v = new ImmutableVector2d();
             Assert.Equal(new(0d, 0d), v);
 
-            v = new ImmutableVector2d(1d, 1d);
+            v = new(1d, 1d);
             Assert.Equal(new(1d, 1d), v);
 
             v = new ImmutableVector2d { X = 1d, Y = 1d, };
             Assert.Equal(new(1d, 1d), v);
 
-            v = new ImmutableVector2d(1d, 1d);
+            v = new(1d, 1d);
             v = v.Double();
             Assert.Equal(new(2d, 2d), v);
 
-            Assert.Equal(Math.Sqrt(2d), new ImmutableVector2d(1d, 1d).Distance);
+            v = new(1d, 1d);
+            Assert.Equal(Math.Sqrt(2d), v.Distance);
         }
 
         [Fact]
@@ -46,17 +52,46 @@ namespace CSharpImmutables.Tests
             var v = new VectorRecord2d();
             Assert.Equal(new(0d, 0d), v);
 
-            v = new VectorRecord2d(1d, 1d);
+            v = new(1d, 1d);
             Assert.Equal(new(1d, 1d), v);
 
             v = new VectorRecord2d { X = 1d, Y = 1d, };
             Assert.Equal(new(1d, 1d), v);
 
-            v = new VectorRecord2d(1d, 1d);
+            v = new(1d, 1d);
             v = v.Double();
             Assert.Equal(new(2d, 2d), v);
 
-            Assert.Equal(Math.Sqrt(2d), new VectorRecord2d(1d, 1d).Distance);
+            v = new(1d, 1d);
+            Assert.Equal(Math.Sqrt(2d), v.Distance);
+        }
+
+        [Fact]
+        public void MutableLine2d()
+        {
+            // Let's create a line and move it a little bit.
+            var l = new MutableLine2d(new(1d, 1d), new(2d, 2d));
+            l.Move(1d, 1d);
+
+            // Check if moving was successful
+            Assert.Equal(new(2d, 2d), l.Start);
+            Assert.Equal(new(3d, 3d), l.End);
+        }
+
+        [Fact]
+        public void ImmutableLine2d()
+        {
+            // Let's create a line and move it a little bit.
+            var l = new ImmutableLine2d(new(1d, 1d), new(2d, 2d));
+            l.Move(1d, 1d);
+
+            // You would expect the next two unit tests to work. They don't
+            // because of defensive copying.
+            // Assert.Equal(new(2d, 2d), l.Start);
+            // Assert.Equal(new(3d, 3d), l.End);
+            
+            Assert.Equal(new(1d, 1d), l.Start);
+            Assert.Equal(new(2d, 2d), l.End);
         }
     }
 }
--- CSharpImmutables/CSharpImmutables/ImmutableStructs.cs
+++ CSharpImmutables/CSharpImmutables/ImmutableStructs.cs
@@ -4,9 +4,9 @@ namespace CSharpImmutables
 {
     public struct MutableVector2d : IEquatable<MutableVector2d>
     {
-        // Two mutable properties
-        public double X { get; set; }
-        public double Y { get; set; }
+        // Two mutable fields
+        public double X;
+        public double Y;
 
         public MutableVector2d(double x, double y) => (X, Y) = (x, y);
 
@@ -18,7 +18,13 @@ namespace CSharpImmutables
         // Try adding `readonly` -> will lead to an error
         public /*readonly*/ void Double() { X *= 2d; Y *= 2d; }
 
+        // Note that the following method changes the state of the vector.
+        // This will be important later.
+        public void Move(double x, double y) { X += x; Y += y; }
+
         public readonly override int GetHashCode() => HashCode.Combine(X, Y);
+
+        public readonly override string ToString() => $"{{ X: {X}, Y: {Y} }}";
 
         #region Implementation of equatable
         public override readonly bool Equals([NotNullWhen(true)] object? v) => v is MutableVector2d && Equals(v);
@@ -57,11 +63,56 @@ namespace CSharpImmutables
         #endregion
     }
 
+    // Now the same with a `readoly record struct`. Much shorter :)
     public readonly record struct VectorRecord2d(double X, double Y)
     {
         public double Distance => Math.Sqrt(X * X + Y * Y);
 
         // Note that `Double` returns a new instance of the vector
         public VectorRecord2d Double() => new(X * 2d, Y * 2d);
+    }
+
+    // Let's build a naive mutable line. Check the unit tests, it works.
+    public struct MutableLine2d
+    {
+        // Try changing field to property. Unit tests fail now. Can you find out why?
+        // Answer: Property getter returns a copy of Start and Move is called on the copy.
+        // Learning: Mutable structs are tricky and you have to be very careful with them
+        public MutableVector2d Start; // { get; set; } 
+        public MutableVector2d End; // { get; set; } 
+
+        public MutableLine2d(MutableVector2d start, MutableVector2d end) => (Start, End) = (start, end);
+
+        public void Move(double x, double y)
+        {
+            Start.Move(x, y);
+            End.Move(x, y);
+        }
+    }
+
+    // Note that the following struct is shallow immutable.
+    // It is immutable because of `readonly`, but its members are not.
+    public readonly struct ImmutableLine2d
+    {
+        public readonly MutableVector2d Start;
+        public readonly MutableVector2d End;
+
+        public ImmutableLine2d(MutableVector2d start, MutableVector2d end) => (Start, End) = (start, end);
+
+        public void Move(double x, double y)
+        {
+            // Note that we call a method here that mutates the states of
+            // Start and End. However, we are in a readonly class.
+            // Therefore, C# will create a "defensive copy". The copy will be
+            // changed, not the original value. Therefore, any changes that
+            // Move does will be lost. Try in debugger.
+
+            // Defensive copying is done because:
+            // * Start is a readonly field and
+            // * type of Start (MutableVector2d) is a non-readonly struct
+
+            Start.Move(x, y);
+            End.Move(x, y);
+        }
     }
 }
