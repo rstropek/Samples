4da804c3a9d45992a4007840c1d057131a9c0d97
Rainer Stropek <rainer@software-architects.at>
2021-09-19 11:39:39 +0200

    Update date/time improvements sample

--- DotNet6/DateTimeImprovements/DateOnlyTests.cs
+++ DotNet6/DateTimeImprovements/DateOnlyTests.cs
@@ -1,75 +1,71 @@
-using System;
-using System.Text.Json;
+namespace DateTimeImprovements;
 using Xunit;
 
-namespace DateTimeImprovements
+public class DateOnlyTests
 {
-    public class DateOnlyTests
+    [Fact]
+    public void Creation()
     {
-        [Fact]
-        public void Creation()
-        {
-            _ = new DateOnly(2021, 7, 25);
+        _ = new DateOnly(2021, 7, 25);
 
-            // Note: DateOnly is internally an immutable int (can be read using DayNumber property)
-            // representing days since Jan. 1st 0001. For details see implementation at
-            // https://source.dot.net/#System.Private.CoreLib/DateOnly.cs
-            Assert.Equal(new DateOnly(1, 1, 1), DateOnly.FromDayNumber(0));
-        }
+        // Note: DateOnly is internally an immutable int (can be read using DayNumber property)
+        // representing days since Jan. 1st 0001. For details see implementation at
+        // https://source.dot.net/#System.Private.CoreLib/DateOnly.cs
+        Assert.Equal(new DateOnly(1, 1, 1), DateOnly.FromDayNumber(0));
+    }
 
-        [Fact]
-        public void Parsing()
-        {
-            _ = DateOnly.Parse("2021-07-25");
-            _ = DateOnly.ParseExact("2021-07-25", "yyyy-MM-dd");
+    [Fact]
+    public void Parsing()
+    {
+        _ = DateOnly.Parse("2021-07-25");
+        _ = DateOnly.ParseExact("2021-07-25", "yyyy-MM-dd");
 
-            // Note: Support for span parsing
-            _ = DateOnly.Parse("2021-07-25".AsSpan());
+        // Note: Support for span parsing
+        _ = DateOnly.Parse("2021-07-25".AsSpan());
 
-            Assert.True(DateOnly.TryParse("2021-07-25", out var _));
+        Assert.True(DateOnly.TryParse("2021-07-25", out var _));
 
-            // The following line does not at the time of writing. No support
-            // for DateOnly/TimeOnly in JsonSerializer yet. Read more at
-            // https://github.com/dotnet/runtime/issues/53539
-            // _ = JsonSerializer.Deserialize<DateOnly>("\"2021-07-25\"");
-        }
+        // The following line does not at the time of writing. No support
+        // for DateOnly/TimeOnly in JsonSerializer yet. Read more at
+        // https://github.com/dotnet/runtime/issues/53539
+        // _ = JsonSerializer.Deserialize<DateOnly>("\"2021-07-25\"");
+    }
 
-        [Fact]
-        public void Conversion()
-        {
-            var date = DateOnly.FromDateTime(DateTime.Today);
+    [Fact]
+    public void Conversion()
+    {
+        var date = DateOnly.FromDateTime(DateTime.Today);
 
-            DateTime dateTime = date.ToDateTime(TimeOnly.MinValue);
-            Assert.Equal(date.DayOfYear, dateTime.DayOfYear);
+        DateTime dateTime = date.ToDateTime(TimeOnly.MinValue);
+        Assert.Equal(date.DayOfYear, dateTime.DayOfYear);
 
-            Assert.Equal("2021-07-25", new DateOnly(2021, 7, 25).ToString("o")); // (ISO 8601 format)
-        }
+        Assert.Equal("2021-07-25", new DateOnly(2021, 7, 25).ToString("o")); // (ISO 8601 format)
+    }
 
-        [Fact]
-        public void Manipulation()
-        {
-            DateOnly date = new(2021, 1, 31);
+    [Fact]
+    public void Manipulation()
+    {
+        DateOnly date = new(2021, 1, 31);
 
-            var nextDate = date.AddDays(1);
-            Assert.Equal(new DateOnly(2021, 2, 1), nextDate);
+        var nextDate = date.AddDays(1);
+        Assert.Equal(new DateOnly(2021, 2, 1), nextDate);
 
-            var nextMonth = date.AddMonths(1);
-            Assert.Equal(new DateOnly(2021, 2, 28), nextMonth);
-        }
+        var nextMonth = date.AddMonths(1);
+        Assert.Equal(new DateOnly(2021, 2, 28), nextMonth);
+    }
 
-        [Fact]
-        public void Properties()
-        {
-            DateOnly date = new(2021, 7, 25);
+    [Fact]
+    public void Properties()
+    {
+        DateOnly date = new(2021, 7, 25);
 
-            Assert.Equal(2021, date.Year);
-            Assert.Equal(7, date.Month);
-            Assert.Equal(25, date.Day);
-            Assert.Equal(DayOfWeek.Sunday, date.DayOfWeek);
-            Assert.Equal(737995, date.DayNumber);
+        Assert.Equal(2021, date.Year);
+        Assert.Equal(7, date.Month);
+        Assert.Equal(25, date.Day);
+        Assert.Equal(DayOfWeek.Sunday, date.DayOfWeek);
+        Assert.Equal(737995, date.DayNumber);
 
-            Assert.Equal(DateOnly.MinValue, new DateOnly(1, 1, 1));
-            Assert.Equal(DateOnly.MaxValue, new DateOnly(9999, 12, 31));
-        }
+        Assert.Equal(DateOnly.MinValue, new DateOnly(1, 1, 1));
+        Assert.Equal(DateOnly.MaxValue, new DateOnly(9999, 12, 31));
     }
 }
--- DotNet6/DateTimeImprovements/TimeOnlyTests.cs
+++ DotNet6/DateTimeImprovements/TimeOnlyTests.cs
@@ -1,77 +1,74 @@
-using System;
+namespace DateTimeImprovements;
 using Xunit;
 
-namespace DateTimeImprovements
+public class TimeOnlyTests
 {
-    public class TimeOnlyTests
+    [Fact]
+    public void Creation()
     {
-        [Fact]
-        public void Creation()
-        {
-            _ = new TimeOnly(17, 30, 42, 999);
+        _ = new TimeOnly(17, 30, 42, 999);
 
-            // Note: TimeOnly is internally an immutable long (can be read using Ticks property)
-            // representing 100-nanosecond intervals since beginning of day
-            // https://source.dot.net/#System.Private.CoreLib/TimeOnly.cs
-            Assert.Equal(0, TimeOnly.MinValue.Ticks);
-        }
+        // Note: TimeOnly is internally an immutable long (can be read using Ticks property)
+        // representing 100-nanosecond intervals since beginning of day
+        // https://source.dot.net/#System.Private.CoreLib/TimeOnly.cs
+        Assert.Equal(0, TimeOnly.MinValue.Ticks);
+    }
 
-        [Fact]
-        public void Parsing()
-        {
-            _ = TimeOnly.Parse("17:30:42.999");
-            _ = DateOnly.ParseExact("17:30:42.999", @"HH\:mm\:ss\.fff");
+    [Fact]
+    public void Parsing()
+    {
+        _ = TimeOnly.Parse("17:30:42.999");
+        _ = DateOnly.ParseExact("17:30:42.999", @"HH\:mm\:ss\.fff");
 
-            // Note: Support for span parsing
-            _ = TimeOnly.Parse("17:30:42.999".AsSpan());
+        // Note: Support for span parsing
+        _ = TimeOnly.Parse("17:30:42.999".AsSpan());
 
-            Assert.True(TimeOnly.TryParse("17:30:42.999", out var _));
+        Assert.True(TimeOnly.TryParse("17:30:42.999", out var _));
 
-            // The following line does not at the time of writing. No support
-            // for DateOnly/TimeOnly in JsonSerializer yet. Read more at
-            // https://github.com/dotnet/runtime/issues/53539
-            // _ = JsonSerializer.Deserialize<TimeOnly>("\"17:30:42.999\"");
-        }
+        // The following line does not at the time of writing. No support
+        // for DateOnly/TimeOnly in JsonSerializer yet. Read more at
+        // https://github.com/dotnet/runtime/issues/53539
+        // _ = JsonSerializer.Deserialize<TimeOnly>("\"17:30:42.999\"");
+    }
 
-        [Fact]
-        public void Conversion()
-        {
-            var time = TimeOnly.FromTimeSpan(TimeSpan.FromHours(1));
-            _ = TimeOnly.FromDateTime(DateTime.Now);
+    [Fact]
+    public void Conversion()
+    {
+        var time = TimeOnly.FromTimeSpan(TimeSpan.FromHours(1));
+        _ = TimeOnly.FromDateTime(DateTime.Now);
 
-            TimeSpan timespan = time.ToTimeSpan();
-            Assert.Equal(time.Ticks, timespan.Ticks);
+        TimeSpan timespan = time.ToTimeSpan();
+        Assert.Equal(time.Ticks, timespan.Ticks);
 
-            Assert.Equal("01:00:00", time.ToString(@"HH\:mm\:ss"));
-        }
+        Assert.Equal("01:00:00", time.ToString(@"HH\:mm\:ss"));
+    }
 
-        [Fact]
-        public void Manipulation()
-        {
-            TimeOnly time = new(17, 30, 42, 999);
+    [Fact]
+    public void Manipulation()
+    {
+        TimeOnly time = new(17, 30, 42, 999);
 
-            var nextMinute = time.AddMinutes(1);
-            Assert.Equal(new TimeOnly(17, 31, 42, 999), nextMinute);
+        var nextMinute = time.AddMinutes(1);
+        Assert.Equal(new TimeOnly(17, 31, 42, 999), nextMinute);
 
-            var nextHour = time.AddHours(1);
-            Assert.Equal(new TimeOnly(18, 30, 42, 999), nextHour);
+        var nextHour = time.AddHours(1);
+        Assert.Equal(new TimeOnly(18, 30, 42, 999), nextHour);
 
-            var nextSecond = time.Add(TimeSpan.FromSeconds(1));
-            Assert.Equal(new TimeOnly(17, 30, 43, 999), nextSecond);
-        }
+        var nextSecond = time.Add(TimeSpan.FromSeconds(1));
+        Assert.Equal(new TimeOnly(17, 30, 43, 999), nextSecond);
+    }
 
-        [Fact]
-        public void Properties()
-        {
-            TimeOnly time = new(17, 30, 42, 999);
+    [Fact]
+    public void Properties()
+    {
+        TimeOnly time = new(17, 30, 42, 999);
 
-            Assert.Equal(17, time.Hour);
-            Assert.Equal(30, time.Minute);
-            Assert.Equal(42, time.Second);
-            Assert.Equal(999, time.Millisecond);
-            Assert.Equal(630429990000, time.Ticks);
+        Assert.Equal(17, time.Hour);
+        Assert.Equal(30, time.Minute);
+        Assert.Equal(42, time.Second);
+        Assert.Equal(999, time.Millisecond);
+        Assert.Equal(630429990000, time.Ticks);
 
-            Assert.Equal(TimeOnly.MinValue, new TimeOnly(0, 0));
-        }
+        Assert.Equal(TimeOnly.MinValue, new TimeOnly(0, 0));
     }
 }
