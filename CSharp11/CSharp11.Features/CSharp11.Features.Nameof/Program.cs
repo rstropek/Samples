17bac3a715d8edcbef2071302cc1ad3b704c4141
Rainer Stropek <rainer@timecockpit.com>
2022-10-08 19:57:20 +0200

    Add nameof sample

---
+++ CSharp11/CSharp11.Features/CSharp11.Features.Nameof/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+var x = 5;
+Verify.Lower(x * 2, Convert.ToInt32(Math.Floor(Math.PI)));
+
+public static class Verify
+{
+    public static void Lower(int argument, int maxValue,
+        [CallerArgumentExpression(nameof(argument))] string? argumentExpression = null,
+        [CallerArgumentExpression(nameof(maxValue))] string? maxValueExpression = null)
+    {
+        if (argument > maxValue)
+        {
+            throw new ArgumentOutOfRangeException(nameof(argument),
+                $"{argumentExpression} must be lower or equal {maxValueExpression}");
+        }
+    }
+}
