80bd67c3b515c3aaa4cea6119a195cbe69e88583
Rainer <rainer@software-architects.at>
2024-02-27 07:12:22 +0100

    Update sample

--- BeyondREST/BeyondREST/SignalRIntroduction/Program.cs
+++ BeyondREST/BeyondREST/SignalRIntroduction/Program.cs
@@ -1,26 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRDrawingServer.Hubs;
+using SignalRIntroduction;
 
-namespace SignalRIntroduction
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<MathAlgorithms>();
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddCors();
+var app = builder.Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+app.UseCors(builder => builder.WithOrigins("http://localhost:5000", "http://127.0.0.1:5000").AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
+app.MapHub<GreetingHub>("/hub");
+app.Run();
