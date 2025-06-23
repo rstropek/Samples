53af895c6f5304a693bd832eb4c1c1105a6386c5
Rainer Stropek <rainer@software-architects.at>
2025-06-23 08:41:15 +0000

    Add lambda modifiers sample

---
+++ CSharp14/LambdaModifiers/Program.cs
@@ -0,0 +1,29 @@
+﻿// Before
+TryParseCsv tryParseCsv = (string input, out List<int> result) => {
+    result = [];
+    foreach(var column in input.Split(',')) {
+        if(int.TryParse(column, out int value)) {
+            result.Add(value);
+            return true;
+        }
+    }
+
+    result.Clear();
+    return false;
+};
+
+// New (note: you can leave out types for the parameters)
+TryParseCsv tryParseCsvNew = (input, out result) => {
+    result = [];
+    foreach(var column in input.Split(',')) {
+        if(int.TryParse(column, out int value)) {
+            result.Add(value);
+            return true;
+        }
+    }
+
+    result.Clear();
+    return false;
+};
+
+delegate bool TryParseCsv(string input, out List<int> result);
